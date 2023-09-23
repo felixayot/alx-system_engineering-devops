20c1e480cec81e128e34e5b547cbc964720962dc
thinktank01 <felixayot@gmail.com>
2023-09-23 17:35:02 +0300

    Improve on the Puppet manifest

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Sets up your client SSH configuration file so that you can connect
 # to a server without typing a password.
-file_line { 'ssh_config':
+file_line { 'use the private key ~/.ssh/school':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
 }
-file_line { 'ssh_config':
+file_line { 'decline to authenticate using a password':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
