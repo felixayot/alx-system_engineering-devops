de943ee4b092d71b492849197731bf84b15a9432
thinktank01 <felixayot@gmail.com>
2023-11-07 19:24:57 +0300

    Refactor 2-recurse.py to prevent runtime errors

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
 """Module to query the Reddit API for a list of hot articles."""
 import requests
+import sys
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
+def recurse(subreddit, hot_list=[], after=None):
     """Recursively returns a list of titles of all hot posts
        for a given subreddit.
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-agent":
         "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) \
         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 \
         Mobile Safari/537.36"
     }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    resp = requests.get(url, headers=headers, params=params,
-                        allow_redirects=False)
-    if resp.status_code == 404:
+    try:
+        if after:
+            count = requests.get("https://www.reddit.com/r/{}/hot.json?\
+                                 after={}".format(subreddit, after),
+                                 headers=headers).json().get("data")
+        else:
+            count = requests.get("https://www.reddit.com/r/{}/hot.json".format
+                                 (subreddit),
+                                 headers=headers).json().get("data")
+        hot_list += [dic.get("data").get("title")
+                     for dic in count.get("children")]
+        if count.get("after"):
+            return recurse(subreddit, hot_list, after=count.get("after"))
+        return hot_list
+    except Exception:
         return None
 
-    results = resp.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for i in results.get("children"):
-        hot_list.append(i.get("data").get("title"))
 
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+if __name__ == "__main__":
+    recurse(sys.argv[1])
